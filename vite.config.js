4e85801fd8e5234001e5c322c9d210cdc35b584e
Florian Bardin <fbardin51@gmail.com>
2025-09-08 21:56:19 +0200

    Importation des composants Button, calendrier et popover de shadcnui

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
+import path from "path";
+import { fileURLToPath } from "url";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": "/src",
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
