82f5a454293a81eb694b3e054a44633b6233d0ef
Florian Bardin <fbardin51@gmail.com>
2025-09-08 21:59:08 +0200

    Création du composant PopoverCal

---
+++ src/components/PopoverCal.jsx
@@ -0,0 +1,22 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { BottomCalendar } from "./BottomCalendar";
+
+export function PopoverCal() {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button variant="outline">Open popover</Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <BottomCalendar />
+      </PopoverContent>
+    </Popover>
+  );
+}
