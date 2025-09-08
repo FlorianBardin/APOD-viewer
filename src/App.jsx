4067e768733b89aa44b1178279c5d85fc8ce7fa5
Florian Bardin <fbardin51@gmail.com>
2025-09-08 11:04:56 +0200

    Importation des données relatives à l'API

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
+
+const API_KEY = import.meta.env.VITE_APOD_API_KEY;
+const API_BASE_URL = "https://api.nasa.gov/planetary/apod?api_key=";
+const API_OPTION = {
+  method: "GET",
+  headers: {
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
 
 const App = () => {
   return (
