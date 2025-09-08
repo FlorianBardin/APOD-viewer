d54e5ed9960439fa1f87f76e1e906f36910d3b8d
Florian Bardin <fbardin51@gmail.com>
2025-09-09 00:13:57 +0200

    Mise à jour style du Drawer

--- src/components/InfoDrawer.jsx
+++ src/components/InfoDrawer.jsx
@@ -12,12 +12,12 @@ export default function InfoDrawer({ title, desc }) {
       <Drawer.Portal>
         <Drawer.Overlay className="fixed inset-0 bg-black/40" />
         <Drawer.Content
-          className="left-2 top-2 bottom-2 fixed z-10 outline-none w-[310px] flex"
+          className="left-2 top-2 bottom-2 fixed z-10 outline-none w-[85vw] md:w-[50vw] lg:w-[30vw] flex"
           // The gap between the edge of the screen and the drawer is 8px in this case.
           style={{ "--initial-transform": "calc(100% + 8px)" }}
         >
-          <div className="bg-background h-full w-full grow p-5 flex flex-col rounded-[16px]">
-            <div className="max-w-md mx-auto">
+          <div className="border bg-background h-full w-full grow p-5 flex flex-col items-start rounded-[16px]">
+            <div className="max-w-md">
               <Drawer.Title className="text-accent-foreground font-medium mb-2">
                 {title}
               </Drawer.Title>
