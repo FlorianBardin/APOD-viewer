b464b14a0bb1e3f43e8903f7f17fdbd32fd68f5e
Florian Bardin <fbardin51@gmail.com>
2025-09-09 00:14:12 +0200

    Chargement centré sur la page

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div class="flex flex-row gap-1.5">
-      <div class="w-2 h-2 rounded-full bg-foreground animate-bounce"></div>
-      <div class="w-2 h-2 rounded-full bg-foreground animate-bounce [animation-delay:-.3s]"></div>
-      <div class="w-2 h-2 rounded-full bg-foreground animate-bounce [animation-delay:-.5s]"></div>
+    <div className="w-full h-screen justify-center items-center flex flex-row gap-1.5 z-0">
+      <div className="w-2 h-2 rounded-full bg-foreground animate-bounce"></div>
+      <div className="w-2 h-2 rounded-full bg-foreground animate-bounce [animation-delay:-.3s]"></div>
+      <div className="w-2 h-2 rounded-full bg-foreground animate-bounce [animation-delay:-.5s]"></div>
     </div>
   );
 };
