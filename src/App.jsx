9fc985d71c20ca6067ef268936223e75ebb5a792
Florian Bardin <fbardin51@gmail.com>
2025-09-08 22:58:02 +0200

    Implémentation de l'API dans les composants

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useState, useEffect } from "react";
 import Spinner from "./components/Spinner";
+import { PopoverCal } from "./components/PopoverCal";
+import { BottomCalendar } from "./components/BottomCalendar";
+import InfoDrawer from "./components/InfoDrawer";
 
 const API_KEY = import.meta.env.VITE_APOD_API_KEY;
 const API_BASE_URL = "https://api.nasa.gov/planetary/apod?api_key=";
@@ -44,16 +47,16 @@ const App = () => {
         `Erreur de récupération des données de la photo. : ${error}`
       );
     } finally {
-      setPicIsLoading(true);
+      setPicIsLoading(false);
     }
   };
 
-  // useEffect(() => {
-  //   fetchPictureData();
-  // }, []);
+  useEffect(() => {
+    fetchPictureData(picDate);
+  }, [picDate]);
 
   return (
-    <main>
+    <main className="bg-background">
       {picIsLoading ? (
         <Spinner />
       ) : picErrorMessage ? (
@@ -62,11 +65,14 @@ const App = () => {
         <div
           className="absolute w-full h-screen bg-cover bg-center z-0 bg-primary"
           style={{
-            backgroundImage: "url(fake_display.jpg)",
+            backgroundImage: `url(${picData.hdurl})`,
           }}
         ></div>
       )}
-      <div className="relative w-full h-screen z-10"></div>
+      <div className="relative w-full h-screen z-10 flex flex-col justify-end items-center gap-3.5 pb-10">
+        <PopoverCal date={picDate} setDate={setPicDate} />
+        <InfoDrawer title={picData.title} desc={picData.explanation} />
+      </div>
     </main>
   );
 };
