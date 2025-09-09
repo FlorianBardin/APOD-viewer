aa7ca2921c69fb0fb9631c752c5c2d377ba0e861
Florian Bardin <fbardin51@gmail.com>
2025-09-09 21:01:55 +0200

    Modification de la disposition du loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div className="w-full h-screen justify-center items-center flex flex-row gap-1.5 z-0">
+    <div className="absolute w-full h-screen justify-center items-center flex flex-row gap-1.5 z-0">
       <div className="w-2 h-2 rounded-full bg-foreground animate-bounce"></div>
       <div className="w-2 h-2 rounded-full bg-foreground animate-bounce [animation-delay:-.3s]"></div>
       <div className="w-2 h-2 rounded-full bg-foreground animate-bounce [animation-delay:-.5s]"></div>
